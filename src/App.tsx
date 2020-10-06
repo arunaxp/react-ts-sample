c2b88aba920a00745c0c8bb56888561535581247
arunaxp <arunaxp@gmail.com>
2020-10-06 10:06:51 +0800

    updated app n questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import * as React from "react";
-import "./styles.css";
+import QuestionCard from "./questions/Questions";
 
-export default function App() {
+const App = () => {
+  const [loading, setLoading] = React.useState(false);
+  const [questions, setQuestions] = React.useState([]);
+  const [number, setNumber] = React.useState(0);
+  const [userAnswers, setUserAnswers] = React.useState([]);
+  const [score, setScore] = React.useState(0);
+  const [gameOver, setGameOver] = React.useState(true);
+
+  const startTrivia = () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLElement>) => {};
+
+  const nextQuestion = () => {};
+
   return (
     <div className="App">
-      <span>Sample</span>
+      <h1>React Quiz</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next question
+      </button>
     </div>
   );
-}
+};
+
+export default App;
